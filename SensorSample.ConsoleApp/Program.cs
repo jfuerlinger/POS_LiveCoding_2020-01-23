3eb68e0b69a7ec2bb32d16995a1d3a2200221ae2
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2020-01-24 11:04:54 +0100

    initial commit

---
+++ SensorSample.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SensorSample.Logic;
+
+namespace SensorSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sensor livingRoomTemperature = new Sensor("Living Room", DoSomeLogging);
+            Sensor officeTemperature = new Sensor("Office", DoSomeLogging);
+
+            //livingRoomTemperature.TemperatureMeasured += TemperatureMeasured;
+            //officeTemperature.TemperatureMeasured += TemperatureMeasured;
+
+            for (int i = 0; i < 100; i++)
+            {
+                livingRoomTemperature.MeasureTemperature();
+                officeTemperature.MeasureTemperature();
+            }
+
+        }
+
+        private static void DoSomeLogging(object sender, string text)
+        {
+            Console.WriteLine(text);
+        }
+
+
+        private static void TemperatureMeasured(object sender, double temperature)
+        {
+            Console.WriteLine($"[{sender.ToString()}] Neue Temperatur: {temperature}");
+        }
+    }
+}
---
+++ SensorSample.Logic/Sensor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorSample.Logic
+{
+    public class Sensor
+    {
+        private readonly string _name;
+
+        private EventHandler<string> _loggerMethod;
+        
+        //public event EventHandler<double> TemperatureMeasured;
+
+        public Sensor(string name, EventHandler<string> loggerMethod)
+        {
+            _name = name;
+            _loggerMethod = loggerMethod;
+        }
+
+        public void MeasureTemperature()
+        {
+            Random random = new Random();
+            int temperature = random.Next(1, 40 + 1);
+            _loggerMethod(this, $"Es wurde die Temperatur {temperature} gemessen.");
+            //OnTemperatureMeasured(temperature);
+
+        }
+
+        //protected virtual void OnTemperatureMeasured(double temperature)
+        //{
+        //    //TemperatureMeasured?.Invoke(this, temperature);
+        //}
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
